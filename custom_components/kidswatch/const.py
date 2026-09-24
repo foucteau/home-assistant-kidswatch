55d490edb48534761c9a4be08ac33bfbf8afa643
foucteau <30275408+foucteau@users.noreply.github.com>
2026-09-24 14:41:44 +0200

    Add custom_components/kidswatch/const.py

---
+++ custom_components/kidswatch/const.py
@@ -0,0 +1,9 @@
+DOMAIN = "kidswatch"
+
+CONF_PHONE = "phone"
+CONF_PASSWORD = "password"
+CONF_COUNTRY_CODE = "country_code"
+CONF_M2 = "m2"
+
+DEFAULT_COUNTRY_CODE = "+33"
+BASE_URL_EU = "https://m.kidswatch-eu.com/v2/"
